a38cb400781fcef9d70f86cdc5911c82ab9ee66f
manuelmonterroza873-crypto <manuelmonterroza873@gmail.com>
2026-08-11 21:01:12 -0500

    Add files via upload

---
+++ Vectores/src/main/java/com/mycompany/vectores/Vectores.java
@@ -0,0 +1,15 @@
+package com.mycompany.vectores;
+import java.util.Scanner;
+public class Vectores { 
+    public static void main (String [] args){
+        Scanner teclado =new Scanner (System.in);
+        // Declaracion de vectores
+        int x[]={1,3,10,11};
+        
+        System.out.println("Posicion 0 Contiene"+ x [0]);
+        System.out.println("Posicion 1 Contiene"+ x [1]);
+        System.out.println("Posicion 2 Contiene"+ x [2]);
+        System.out.println("Posicion 3 Contiene"+ x [3]);
+        
+    }
+}
